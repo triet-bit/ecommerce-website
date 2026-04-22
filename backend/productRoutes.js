42d99d5e78b410955cb173e45a843b15af0daf10
Lê Bảo <bao.lesenpai@hcmut.edu.vn>
2026-04-22 14:59:18 +0700

    Thêm API checkout gọi Stored Procedure

--- backend/productRoutes.js
+++ backend/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const pool = require('./db');
-const authenticateJWT = require('./authMiddleware'); // Import middleware bảo vệ route
+const pool = require('../node/db');
+const authenticateJWT = require('../node/authMiddleware'); // Import middleware bảo vệ route
 const router = express.Router();
 
 // GET: Lấy danh sách sản phẩm (Gọi SP)
@@ -58,6 +58,19 @@ router.delete('/:id', authenticateJWT, async (req, res) => {
         res.json({ message: 'Xóa sản phẩm thành công' });
     } catch (error) {
         res.status(500).json({ error: error.message });
+    }
+});
+router.post('/checkout', verifyToken, async (req, res) => {
+    try {
+        const MaKH = req.user.userId; // Lấy từ Token
+        
+        // Gọi thẳng Transaction trong Database để nó tự làm hết
+        await pool.query('CALL Sp_ThanhToanGioHang(?)', [MaKH]);
+        
+        res.status(200).json({ message: "Thanh toán thành công!" });
+    } catch (error) {
+        // Nếu trong SQL báo lỗi (hết tiền, rỗng giỏ), nó sẽ bắn lỗi về đây
+        res.status(400).json({ error: error.message });
     }
 });
 
